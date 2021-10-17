8bd75ed171628b6a38f7d886d1b5ecbcc30a4310
rob <lenon.rob@gmail.com>
2021-10-17 16:39:47 -0500

    Replace all files with new expo init

--- utils/utils.ts
+++
@@ -1,9 +0,0 @@
-import Constants from "expo-constants";
-
-const { manifest } = Constants;
-let uri = "";
-if (__DEV__) {
-  uri = `http://${manifest.debuggerHost.split(":").shift()}:3000`;
-} else {
-  uri = "";
-}
