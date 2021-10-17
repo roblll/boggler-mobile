43025edd115f44f4747816dc8d076aa3afce9314
rob <lenon.rob@gmail.com>
2021-10-17 17:14:08 -0500

    Update App.tsx get WINDOW_WIDTH and render Camera

--- App.tsx
+++ App.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 import { View, Dimensions } from 'react-native';
 import { Camera } from 'expo-camera'
 
+const WINDOW_WIDTH = Dimensions.get('window').width;
+
 export default function App() {
   return (
     <View>
       <StatusBar style="auto" />
+      <Camera 
+        style={{ height: WINDOW_WIDTH, width: WINDOW_WIDTH}}
+      />
     </View>
   );
 }
