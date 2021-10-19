85b3509a777b575568793b7da4b27c3e4b70b641
rob <lenon.rob@gmail.com>
2021-10-19 16:52:51 -0500

    Update utils.ts to update getURI to check that manifest.debuggerHost is not null

--- utils/utils.ts
+++ utils/utils.ts
@@ -3,7 +3,7 @@ import Constants from 'expo-constants';
 export const getURI =  () => {
   const { manifest } = Constants;
   let uri = '';
-  if (__DEV__) {
+  if (__DEV__ && manifest?.debuggerHost) {
     uri = `http://${manifest.debuggerHost.split(":").shift()}:3000/test`;
   } else {
     // prod uri
