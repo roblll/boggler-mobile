32f85dc3a66b4013ecfba31ae37d048211a51af9
rob <lenon.rob@gmail.com>
2021-10-22 17:25:02 -0500

    Add /components/WordsList.tsx

---
+++ components/WordsList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WordsList = () => {
+  return (
+    <View>
+      <Text>Words List</Text>
+    </View>
+  );
+}
+
+export default WordsList;
