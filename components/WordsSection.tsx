160c5d4cb73f89f8230e748aed9755461d1846e9
rob <lenon.rob@gmail.com>
2021-11-03 14:56:10 -0500

    Add componets/WordsSection.tsx

---
+++ components/WordsSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WordsSection = () => {
+  return (
+    <View>
+      <Text>Words Section</Text>
+    </View>
+  );
+}
+
+export default WordsSection;
