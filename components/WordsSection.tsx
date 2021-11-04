5317447481db573afc25ba79ca97778218872f2e
rob <lenon.rob@gmail.com>
2021-11-04 16:03:00 -0500

    Update WordsSection.tsx to add style

--- components/WordsSection.tsx
+++ components/WordsSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 type Props = {
   wordsLength: string;
@@ -13,11 +13,17 @@ const WordsSection: React.FC<Props> = ({ wordsLength, words }) => {
   })
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{`${wordsLength} letter words`}</Text>
       {wordsList}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10
+  }
+})
 
 export default WordsSection;
