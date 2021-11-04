c1f7c1ad15aa8e3fcefe8bb6b47491970a6a80af
rob <lenon.rob@gmail.com>
2021-11-04 16:09:16 -0500

    Add /components/SolveButton.tsx

---
+++ components/SolveButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SolveButton = () => {
+  return (
+    <View>
+      <Text>Solve</Text>
+    </View>
+  );
+}
+
+export default SolveButton;
