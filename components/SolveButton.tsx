7e5463ac15467ac03bf9dc525d5ea8b14a92fe7b
rob <lenon.rob@gmail.com>
2021-11-05 14:51:00 -0500

    Update SolveButton.tsx to add style

--- components/SolveButton.tsx
+++ components/SolveButton.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const SolveButton = () => {
   return (
-    <View>
-      <Text>Solve</Text>
+    <View style={styles.container}>
+      <Text style={styles.buttonText}>SOLVE</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#56d7f4',
+    padding: 10,
+    borderColor: 'white',
+    borderWidth: 100 * .01,
+  },
+  buttonText: {
+    color: '#000',
+  },
+})
 
 export default SolveButton;
