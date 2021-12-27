70d8f4144e448b5ea40e6b3244526183b64e983e
rob <lenon.rob@gmail.com>
2021-12-26 18:06:01 -0600

    Update Board.tsx to display board letters per row

--- components/Board.tsx
+++ components/Board.tsx
@@ -6,12 +6,29 @@ type Props = {
 }
 
 const Board: React.FC<Props> = ({board}) => {
+  let row1: JSX.Element[] = []
+  board[0].forEach(letter => {
+    row1.push(<Text>{letter}</Text>)
+  });
+  let row2: JSX.Element[] = []
+  board[1].forEach(letter => {
+    row2.push(<Text>{letter}</Text>)
+  });
+  let row3: JSX.Element[] = []
+  board[2].forEach(letter => {
+    row3.push(<Text>{letter}</Text>)
+  });
+  let row4: JSX.Element[] = []
+  board[3].forEach(letter => {
+    row4.push(<Text>{letter}</Text>)
+  });
+
   return (
     <View>
-      <Text>{board[0].join(" | ")}</Text>
-      <Text>{board[1].join(" | ")}</Text>
-      <Text>{board[2].join(" | ")}</Text>
-      <Text>{board[3].join(" | ")}</Text>
+      <View>{row1}</View>
+      <View>{row2}</View>
+      <View>{row2}</View>
+      <View>{row2}</View>
     </View>
   );
 }
