a3fcc45e4e3f395d7488b8d57ddd06031f8a4b2c
rob <lenon.rob@gmail.com>
2022-02-19 17:03:35 -0600

    Add EditDice.tsx

---
+++ components/EditDice.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const EditDice = () => {
+  return (
+    <View>
+      <Text>Edit dice</Text>
+    </View>
+  );
+}
+
+export default EditDice;
