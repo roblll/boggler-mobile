990f7d8f36808ec1403e0a2419a4f7cd864cd1c7
rob <lenon.rob@gmail.com>
2022-02-18 21:43:50 -0600

    Update EditBoard.tsx to board components

--- components/EditBoard.tsx
+++ components/EditBoard.tsx
@@ -14,7 +14,33 @@ const EditBoard: React.FC<Props> = ({ updateBoard }) => {
   return (
     <View style={styles.container}>
       <View style={styles.boardSection}>
-        <Text>Edit Board</Text>
+        <View style={styles.board}>
+          <View style={styles.row}>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+            <View style={styles.cell}></View>
+          </View>
+        </View>
+        <View style={styles.bottomBorder} />
       </View>
       <View style={styles.confirmSection}>
         <Button onClick={updateBoard} size={WINDOW_WIDTH * .25} icon='check-bold' />
@@ -40,6 +66,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignContent: 'center',
+  },
+  board: {
+    width: WINDOW_WIDTH * .8,
+    height: WINDOW_WIDTH * .8,
+    backgroundColor: '#15343b',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'flex-end',
+  },
+  cell: {
+    backgroundColor: 'white',
+    width: WINDOW_WIDTH * .8 * .25 * .9,
+    height: WINDOW_WIDTH * .8 * .25 * .9,
+  },
+  bottomBorder: {
+    // backgroundColor: '#f4d456',
+    backgroundColor: '#15343b',
+    height: WINDOW_WIDTH * .8 * .25 * .1,
+    width: WINDOW_WIDTH * .8,
   }
 })
 
