2f487d49b4fa2c47e63230d118c15fb30f7f3895
rob <lenon.rob@gmail.com>
2022-02-20 13:47:21 -0600

    Update EditDice.tsx to import and use ScrollPicker

--- components/EditDice.tsx
+++ components/EditDice.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
+import ScrollPicker from '../components/ScrollPicker';
+
 type Props = {
   close: (letter: string) => void;
   letter: string;
@@ -9,6 +11,24 @@ type Props = {
 const EditDice: React.FC<Props> = ({ close, letter }) => {
   return (
     <View>
+      <View style={{height: 200, width: 200}}>
+        <ScrollPicker
+          dataSource={['A', 'B', 'C', 'D', 'E', 'F']}
+          selectedIndex={0}
+          renderItem={(data, index) => {
+            return (
+              <Text>{data}</Text>
+            )
+          }}
+          // onValueChange={(data, selectedIndex) => {
+          //   setMinRank(selectedIndex)
+          // }}
+          wrapperHeight={200}
+          wrapperColor='#fff'
+          itemHeight={200 / 3}
+          highlightColor='#48CFAD'
+        />
+      </View>
       <Text>{letter}</Text>
       <Button title='hide' onPress={() => close('a') }/>
     </View>
